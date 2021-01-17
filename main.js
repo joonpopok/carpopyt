48a6c607ef710292afa808f57bb47b356c85ed37
joonpopok <70682592+joonpopok@users.noreply.github.com>
2021-01-17 15:34:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function numberWithCommas(x) {
 
 // Request play list items
 function requestPlaylist(playlistId) {
-    const maxResults = 99999999999999;
+    const maxResults = 99990;
     const playlistURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${apiKey}&playlistId=${playlistId}&part=snippet&maxResults=${maxResults}`;
 
     fetch(playlistURL)
